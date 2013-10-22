5cea5e8206e4d21f947be2ae4ede69b662efe6c9
Olli Etuaho <olli.etuaho@gmail.com>
2013-10-22 21:22:12 +0300

    Don't beep on style errors

--- check_style.bat
+++ check_style.bat
@@ -1,4 +1,4 @@
 @pushd %~dp0
-gjslint --max_line_length 120 *.js
-gjslint --max_line_length 120 unit_tests/*.js
+gjslint --max_line_length 120 --nobeep *.js
+gjslint --max_line_length 120 --nobeep unit_tests/*.js
 @popd
