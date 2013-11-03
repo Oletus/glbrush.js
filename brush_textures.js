fe21af5aee244e2cb8b95054acca88c88077a2e3
Olli Etuaho <olli.etuaho@gmail.com>
2013-11-03 15:46:54 +0200

    Limit textured brush inside a circle

--- brush_textures.js
+++ brush_textures.js
@@ -12,6 +12,10 @@ var GLBrushTextures = function(gl, glManager) {
     this.gl = gl;
     this.glManager = glManager;
     this.textures = [];
+    this.helperCanvas = document.createElement('canvas');
+    this.helperCanvas.width = 0;
+    this.helperCanvas.height = 0;
+    this.helperCtx = this.helperCanvas.getContext('2d');
 };
 
 /**
@@ -19,9 +23,26 @@ var GLBrushTextures = function(gl, glManager) {
  * channel. Both dimensions of the image must be equal and powers of two.
  */
 GLBrushTextures.prototype.addTexture = function(imageSource) {
+    var w = imageSource.width;
+    if (w !== this.helperCanvas.width) {
+        this.helperCanvas.width = w;
+        this.helperCanvas.height = w;
+    }
+    //this.helperCtx.resetClip();
+    this.helperCtx.clearRect(0, 0, w, w);
+    this.helperCtx.drawImage(imageSource, 0, 0);
+    // Mask the brush inside a circle to guarantee that even when rotated it will stay within expected boundaries
+    this.helperCtx.beginPath();
+    this.helperCtx.arc(w * 0.5, w * 0.5, w * 0.5 - 2, 0, Math.PI * 2);
+    this.helperCtx.moveTo(0, 0);
+    this.helperCtx.lineTo(0, w);
+    this.helperCtx.lineTo(w, w);
+    this.helperCtx.lineTo(w, 0);
+    this.helperCtx.lineTo(0, 0);
+    this.helperCtx.fill('evenodd');
     var tex = this.gl.createTexture();
     this.gl.bindTexture(this.gl.TEXTURE_2D, tex);
-    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, imageSource);
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.helperCanvas);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_LINEAR);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
