04c7d7a02393db5213ff1c680449ca0a1a7459bf
Olli Etuaho <olli.etuaho@gmail.com>
2014-01-24 01:12:12 +0200

    Add tests for new util2d functionality

--- unit_tests/util2d_spec.js
+++ unit_tests/util2d_spec.js
@@ -173,6 +173,14 @@ describe('util2d', function() {
             expect(vec.y).toBe(2);
         });
 
+        it('can be set from another Vec2', function() {
+            var vecA = new Vec2(1, 2);
+            var vecB = new Vec2(3, 4);
+            vecA.setVec2(vecB);
+            expect(vecA.x).toBe(3);
+            expect(vecA.y).toBe(4);
+        });
+
         it('calculates its length', function() {
             var vec = new Vec2(3, 4);
             expect(vec.length()).toBeNear(5, 0.001);
@@ -190,6 +198,13 @@ describe('util2d', function() {
             expect(vec.length()).toBeNear(1, 0.001);
         });
 
+        it('rounds its coordinates', function() {
+            var vec = new Vec2(3.33, 4.8);
+            vec.round();
+            expect(vec.x).toBe(3);
+            expect(vec.y).toBe(5);
+        });
+
         it('calculates a dot product', function() {
             var vecA = new Vec2(1.2, 3.4);
             var vecB = new Vec2(8.7, 6.5);
@@ -202,6 +217,14 @@ describe('util2d', function() {
             vec.scale(3.4);
             expect(vec.x).toBeNear(3.4, 0.00001);
             expect(vec.y).toBeNear(6.8, 0.00001);
+        });
+
+        it('translates', function() {
+                var vecA = new Vec2(1, 3);
+                var vecB = new Vec2(5, 7);
+                vecA.translate(vecB);
+                expect(vecA.x).toBe(6);
+                expect(vecA.y).toBe(10);
         });
 
         it('calculates a slope to another Vec2', function() {
@@ -237,6 +260,46 @@ describe('util2d', function() {
             var vecB = new Vec2(8.7, 6.5);
             var vecC = new Vec2(9.0, 1.2);
             expect(vecC.distanceToLine(vecA, vecB)).toBeNear(5.0126811, 0.001);
+        });
+
+        it('calculates its angle to the positive x axis', function() {
+            var vecA = new Vec2(1.0, 0.0);
+            expect(vecA.angle()).toBeNear(0, 0.0001);
+            var vecB = new Vec2(0.0, 1.0);
+            expect(vecB.angle()).toBeNear(Math.PI * 0.5, 0.0001);
+            var vecC = new Vec2(-1.0, 0.0);
+            expect(vecC.angle()).toBeNear(Math.PI, 0.0001);
+            var vecD = new Vec2(0.0, -1.0);
+            expect(vecD.angle()).toBeNear(Math.PI * 1.5, 0.0001);
+            var vecE = new Vec2(1.0, 1.0);
+            expect(vecE.angle()).toBeNear(Math.PI * 0.25, 0.0001);
+        });
+
+        it('calculates its angle to another vector', function() {
+            var vecA = new Vec2(1.0, 0.0);
+            expect(vecA.angleFrom(vecA)).toBeNear(0, 0.0001);
+            var vecB = new Vec2(0.0, 1.0);
+            expect(vecB.angleFrom(vecA)).toBeNear(Math.PI * 0.5, 0.0001);
+            expect(vecA.angleFrom(vecB)).toBeNear(-Math.PI * 0.5, 0.0001);
+            var vecC = new Vec2(-1.0, 0.0);
+            expect(vecC.angleFrom(vecA)).toBeNear(Math.PI, 0.0001);
+            expect(vecC.angleFrom(vecB)).toBeNear(Math.PI * 0.5, 0.0001);
+            expect(vecB.angleFrom(vecC)).toBeNear(-Math.PI * 0.5, 0.0001);
+            var vecD = new Vec2(0.0, -1.0);
+            expect(vecD.angleFrom(vecA)).toBeNear(Math.PI * 1.5, 0.0001);
+            expect(vecD.angleFrom(vecB)).toBeNear(Math.PI, 0.0001);
+            expect(vecD.angleFrom(vecC)).toBeNear(Math.PI * 0.5, 0.0001);
+            expect(vecC.angleFrom(vecD)).toBeNear(-Math.PI * 0.5, 0.0001);
+        });
+
+        it('rotates', function() {
+            for (var i = 0; i < 10; ++i) {
+                var vecA = new Vec2(1.0, 0.0);
+                var vecB = new Vec2(1.0, 0.0);
+                vecB.rotate(i * 0.2 * Math.PI);
+                vecA.rotate(i * 0.1 * Math.PI);
+                expect(vecB.angleFrom(vecA)).toBeNear(i * 0.1 * Math.PI, 0.0001);
+            }
         });
     });
 
