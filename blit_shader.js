dd79cb01c2429996295f4e997db99b0236eb1a4e
Olli Etuaho <olli.etuaho@gmail.com>
2019-05-20 21:52:47 +0300

    Use template strings in blit_shader.js
    This makes the shader code more readable.

--- blit_shader.js
+++ blit_shader.js
@@ -8,64 +8,64 @@ var blitShader = {};
 /**
  * Fragment shader source for converting a float monochrome raster to color.
  */
-blitShader.convertSimpleSrc = '  precision highp float;\n' +
-'  uniform sampler2D uSrcTex;\n' +
-'  varying vec2 vTexCoord;\n' +
-'  uniform vec4 uColor;\n' +
-'  void main(void) {\n' +
-'    vec4 src = texture2D(uSrcTex, vTexCoord);\n' +
-'    float a = src.w * uColor.w;\n' +
-'    gl_FragColor = vec4(uColor.xyz * a, a); // premultiply\n' +
-'  }';
+blitShader.convertSimpleSrc = `  precision highp float;
+  uniform sampler2D uSrcTex;
+  varying vec2 vTexCoord;
+  uniform vec4 uColor;
+  void main(void) {
+    vec4 src = texture2D(uSrcTex, vTexCoord);
+    float a = src.w * uColor.w;
+    gl_FragColor = vec4(uColor.xyz * a, a); // premultiply
+  }`;
 
 /**
  * Fragment shader source for converting a monochrome raster stored in red and
  * green channels to color.
  */
-blitShader.convertRedGreenSrc = '  precision highp float;\n' +
-'  uniform sampler2D uSrcTex;\n' +
-'  varying vec2 vTexCoord;\n' +
-'  uniform vec4 uColor;\n' +
-'  void main(void) {\n' +
-'    vec4 src = texture2D(uSrcTex, vTexCoord);\n' +
-'    float a = (src.x + src.y / 256.0) * uColor.w;\n' +
-'    gl_FragColor = vec4(uColor.xyz * a, a); // premultiply\n' +
-'  }';
+blitShader.convertRedGreenSrc = `  precision highp float;
+  uniform sampler2D uSrcTex;
+  varying vec2 vTexCoord;
+  uniform vec4 uColor;
+  void main(void) {
+    vec4 src = texture2D(uSrcTex, vTexCoord);
+    float a = (src.x + src.y / 256.0) * uColor.w;
+    gl_FragColor = vec4(uColor.xyz * a, a); // premultiply
+  }`;
 
 /**
  * Fragment shader source for a straight-up blit.
  */
-blitShader.blitSrc = '  precision highp float;\n' +
-'  uniform sampler2D uSrcTex;\n' +
-'  varying vec2 vTexCoord;\n' +
-'  void main(void) {\n' +
-'    gl_FragColor = texture2D(uSrcTex, vTexCoord);\n' +
-'  }';
+blitShader.blitSrc = `  precision highp float;
+  uniform sampler2D uSrcTex;
+  varying vec2 vTexCoord;
+  void main(void) {
+    gl_FragColor = texture2D(uSrcTex, vTexCoord);
+  }`;
 
 /**
  * Vertex shader source for blitting/conversion/compositing.
  */
-blitShader.blitVertSrc = '  precision highp float;\n' +
-'  attribute vec2 aVertexPosition;\n' +
-'  varying vec2 vTexCoord;\n' +
-'  void main(void) {\n' +
-'    vTexCoord = vec2((aVertexPosition.x + 1.0) * 0.5, (aVertexPosition.y + 1.0) * 0.5);\n' +
-'    gl_Position = vec4(aVertexPosition, 0.0, 1.0);\n' +
-'  }';
+blitShader.blitVertSrc = `  precision highp float;
+  attribute vec2 aVertexPosition;
+  varying vec2 vTexCoord;
+  void main(void) {
+    vTexCoord = vec2((aVertexPosition.x + 1.0) * 0.5, (aVertexPosition.y + 1.0) * 0.5);
+    gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+  }`;
 
 /**
  * Vertex shader source for drawing scaled/translated image.
  */
-blitShader.blitScaledTranslatedVertSrc = '  precision highp float;\n' +
-'  attribute vec2 aVertexPosition;\n' +
-'  uniform vec2 uScale;\n' +
-'  uniform vec2 uTranslate;\n' +
-'  varying vec2 vTexCoord;\n' +
-'  void main(void) {\n' +
-'    vTexCoord = vec2((aVertexPosition.x + 1.0) * 0.5, (aVertexPosition.y + 1.0) * 0.5);\n' +
-'    vec2 vertexPosition = aVertexPosition * uScale + uTranslate;\n' +
-'    gl_Position = vec4(vertexPosition, 0.0, 1.0);\n' +
-'  }';
+blitShader.blitScaledTranslatedVertSrc = `  precision highp float;
+  attribute vec2 aVertexPosition;
+  uniform vec2 uScale;
+  uniform vec2 uTranslate;
+  varying vec2 vTexCoord;
+  void main(void) {
+    vTexCoord = vec2((aVertexPosition.x + 1.0) * 0.5, (aVertexPosition.y + 1.0) * 0.5);
+    vec2 vertexPosition = aVertexPosition * uScale + uTranslate;
+    gl_Position = vec4(vertexPosition, 0.0, 1.0);
+  }`;
 
 /**
  * Uniform parameters for the conversion shaders.
