7ee9e5c1dcccb4a1d57e7599071f545236cd3290
Olli Etuaho <olli.etuaho@gmail.com>
2019-05-21 11:54:26 +0300

    Change scripts to use Closure compiler API
    The online compiler supports the ES6 features that we are now using.

--- tools/compile_glbrush.py
+++ tools/compile_glbrush.py
@@ -15,8 +15,8 @@ def glbrush_root_path():
 def lib_js():
     '''Returns a list of absolute paths to glbrush js files containing the library.
     Files are sorted in dependency order.'''
-    glbrush = glbrush_root_path()
-    js_files = js_files_in(glbrush)
+    glbrush_root = glbrush_root_path()
+    js_files = js_files_in(glbrush_root)
     def client_js_key(path):
         # This hardcoded dependency order is a bit ugly, could perhaps be replaced with better dependency management.
         if path.count('util') > 0:
@@ -25,35 +25,48 @@ def lib_js():
             return 'B' + path
         return 'C' + path
     js_files = sorted(js_files, key=client_js_key)
-    return js_files_in(os.path.join(glbrush, 'lib')) + js_files
+    return js_files_in(os.path.join(glbrush_root, 'lib')) + js_files
+
+def compile_with_online_compiler(js_code, compilation_level):
+    import httplib, urllib
+
+    # Define the parameters for the POST request and encode them in
+    # a URL-safe format.
+
+    params = urllib.urlencode([
+        ('js_code', js_code),
+        ('compilation_level', compilation_level),
+        ('output_format', 'text'),
+        ('output_info', 'compiled_code'),
+      ])
+
+    # Always use the following value for the Content-type header.
+    headers = { "Content-type": "application/x-www-form-urlencoded" }
+    conn = httplib.HTTPSConnection('closure-compiler.appspot.com')
+    conn.request('POST', '/compile', params, headers)
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    return data
 
 def compile_glbrush(output_path):
     '''Compile glbrush.js with Closure compiler.
     output_path should include the name of the file.'''
-    restore_dir = os.getcwd()
     file_path = os.path.dirname(os.path.abspath(__file__))
-    compiler_path = os.path.abspath(os.path.join(file_path, '..', 'compile'))
     lib_js_list = lib_js()
-    if not os.path.exists(compiler_path):
-        os.mkdir(compiler_path)
-    os.chdir(compiler_path)
 
-    if not os.path.exists('compiler.jar'):
-        print('Automatically downloading Closure compiler toolchain')
-        from fetch_compiler import fetch_compiler
-        if not fetch_compiler('compiler.jar'):
-            print('Place compiler.jar to directory "compile"')
-            os.chdir(restore_dir)
-            return False
+    lib_js_code = ""
+    for js_file in lib_js_list:
+        source_file = open(js_file, "r")
+        lib_js_code += source_file.read()
+        source_file.close()
 
     # Compile a package that's usable within another app, so WHITESPACE_ONLY
-    command = ['java', '-jar', 'compiler.jar', '--compilation_level', 'WHITESPACE_ONLY', '--js']
-    command += lib_js_list
-    command += ['--js_output_file', output_path]
-    print(' '.join(command))
-    subprocess.call(command)
+    compiled_code = compile_with_online_compiler(lib_js_code, 'WHITESPACE_ONLY')
+    output_file = open(output_path, "w")
+    output_file.write(compiled_code)
+    output_file.close()
 
-    os.chdir(restore_dir)
     return True
 
 if __name__ == '__main__':
